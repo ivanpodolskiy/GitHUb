3cf282d9c69d0a0a4e68744063d368e047715bf0
ivanpodolskiy <stren.25@mail.ru>
2021-12-11 19:29:06 +0300

    Test3

---
+++ test3.swift
@@ -0,0 +1,18 @@
+//
+//  test3.swift
+//  GitHUb
+//
+//  Created by user on 11.12.2021.
+//
+
+import Foundation
+import UIKit
+
+class test3: UIViewController {
+    
+    let test3: String = "test3"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
